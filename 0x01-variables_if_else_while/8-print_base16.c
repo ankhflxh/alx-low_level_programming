14648ea81e1b42a6422aff6426b8a4ca641ae324
ankhflxh <ashleyjonathan555@icloud.com>
2022-12-09 15:35:35 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- hexadecimal
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num3 = '0';
+	char hex = 'a';
+
+	for (num3 = '0'; num3 <= '9; num3++)
+		putchar(num3);
+	for (hex = 'a'; hex <= '9; hex++)
+                putchar(hex);
+
+	putchar('\n');
+	return (0);
+}
