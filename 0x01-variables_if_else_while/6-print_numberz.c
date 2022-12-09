6f7fe497cf926afd633bf7b5fae425de77d706ce
ankhflxh <ashleyjonathan555@icloud.com>
2022-12-09 15:02:18 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main- Prints base 10 numbers
+ *
+ * Return: 0
+ */
+int main (void)
+{
+	int num2 = '0';
+
+	for (num2 = '0'; num2 <= '9'; num2++)
+		putchar(num2);
+
+	putchar('\n');
+	return (0);
+}
