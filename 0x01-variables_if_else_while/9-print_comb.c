bb9b3db8578c3866f0aa20e2db6126f04dfecd31
ankhflxh <ashleyjonathan555@icloud.com>
2022-12-09 15:57:32 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main- prints single numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for(a = 10; a < 20; a++)
+	{
+		putchar((a % 10) + '0');
+		if (a != 19)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
